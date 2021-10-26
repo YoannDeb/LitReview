59f507be190aa00653b8f01b5d34bb0bd564932f
Yoann <yoann.deblois@gmail.com>
2021-10-26 04:56:08 +0200

    func: added user_follows

--- LitReview/reviews/views.py
+++ LitReview/reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from .models import Review, Ticket
+from .models import Review, Ticket, UserFollow
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -9,3 +10,17 @@ def index(request):
         'tickets': tickets
     }
     return render(request, 'reviews/index.html', context)
+
+
+def user_follows(request):
+    followings = UserFollow.objects.filter(user=request.user)
+    followed_bys = UserFollow.objects.filter(followed_user=request.user)
+    if not followings:
+        followings = ["Vous n'avez pas encore d'abonnements"]
+    if not followed_bys:
+        followed_bys = ["Personne ne s'est encore abonné"]
+    context = {
+        'followings': followings,
+        'followed_bys': followed_bys
+    }
+    return render(request, 'reviews/user_follows.html', context)
