8298c8c700587b5092776991f8f64d5b5c31c210
Yoann <yoann.deblois@gmail.com>
2021-10-27 15:22:56 +0200

    func: added ticket_creation and review_creation

--- LitReview/reviews/urls.py
+++ LitReview/reviews/urls.py
@@ -6,5 +6,7 @@ app_name = 'reviews'
 urlpatterns = [
     path('', views.index, name='index'),
     path('my_posts/', views.my_posts, name='my_posts'),
+    path('ticket_creation/', views.ticket_creation, name='ticket_creation'),
+    path('review_creation/', views.review_creation, name='review_creation'),
     path('user_follows/', views.user_follows, name='user_follows'),
 ]
