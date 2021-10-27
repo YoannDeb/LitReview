d36632919d0ba685f36a8f9098679e097e79285c
Yoann <yoann.deblois@gmail.com>
2021-10-28 00:02:54 +0200

    func: Ticket response page added

--- LitReview/reviews/urls.py
+++ LitReview/reviews/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('my_posts/', views.my_posts, name='my_posts'),
     path('ticket_creation/', views.ticket_creation, name='ticket_creation'),
-    path('review_creation/', views.review_creation, name='review_creation'),
+    path('ticket_response/', views.ticket_response, name='ticket_response'),
     path('user_follows/', views.user_follows, name='user_follows'),
 ]
