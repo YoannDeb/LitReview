acb5b9b8201b9bea25bcb421e0b1bd0006997c3e
Yoann <yoann.deblois@gmail.com>
2021-10-29 05:37:34 +0200

    func: ticket_creation + post deletion

--- LitReview/reviews/views.py
+++ LitReview/reviews/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 from .models import Review, Ticket, UserFollow
 from itertools import chain
-from .forms import TicketResponseForm
+from .forms import TicketResponseForm, TicketCreationForm
+from django.views.generic.edit import DeleteView
 
 
 def index(request):
@@ -24,6 +26,12 @@ def index(request):
 
 
 def my_posts(request):
+    if request.method == 'POST':
+        if request.POST.get('ticket_id'):
+            Ticket.objects.get(pk=request.POST.get('ticket_id')).delete()
+        if request.POST.get('review_id'):
+            Review.objects.get(pk=request.POST.get('review_id')).delete()
+
     tickets = Ticket.objects.filter(user=request.user)
     reviews = Review.objects.filter(user=request.user)
 
@@ -36,21 +44,39 @@ def my_posts(request):
     return render(request, 'reviews/my_posts.html', context)
 
 
+class TicketDeleteView(DeleteView):
+    model = Ticket
+    success_url = reverse_lazy('reviews:my_posts')
+
+
 def ticket_creation(request):
-    context = {}
-    return render(request, 'reviews/ticket_creation', context)
+    if request.method == 'POST':
+        form = TicketCreationForm(request.POST, request.FILES)
+        print(form['image'])
+        if form.is_valid():
+            ticket = Ticket(
+                title=form.cleaned_data['title'],
+                description=form.cleaned_data['description'],
+                image=form.cleaned_data.get('image'),
+                user=request.user
+            )
+            print(form.cleaned_data.get('image'))
+            ticket.save()
+
+            return HttpResponseRedirect('/reviews/')
+    else:
+        form = TicketCreationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'reviews/ticket_creation.html', context)
 
 
 def ticket_response(request):
     if request.method == 'POST' and request.POST.get('headline'):
         form = TicketResponseForm(request.POST)
         if form.is_valid():
-            # Create review in database from POST request
-            # headline = form.cleaned_data['headline']
-            # rating = form.cleaned_data['rating']
-            # body = form.cleaned_data['body']
-            # ticket = request.POST.get('ticket_id')
-            # user = request.user.id
             review = Review(
                 headline=form.cleaned_data['headline'],
                 rating=form.cleaned_data['rating'],
@@ -70,6 +96,11 @@ def ticket_response(request):
         'form': form,
     }
     return render(request, 'reviews/ticket_response.html', context)
+
+
+def review_creation(request):
+    context = {}
+    return render(request, 'reviews/index.html', context)
 
 
 def user_follows(request):
