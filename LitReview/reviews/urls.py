9731154b69484d6a8c614728e6c2fe33c055e781
Yoann <yoann.deblois@gmail.com>
2021-10-29 05:35:41 +0200

    func: added review creation page

--- LitReview/reviews/urls.py
+++ LitReview/reviews/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('my_posts/', views.my_posts, name='my_posts'),
     path('ticket_creation/', views.ticket_creation, name='ticket_creation'),
     path('ticket_response/', views.ticket_response, name='ticket_response'),
+    path('review_creation/', views.review_creation, name='review_creation'),
     path('user_follows/', views.user_follows, name='user_follows'),
 ]
