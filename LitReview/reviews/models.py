a4c82cd591ed512f984c8e8ce37a7e5dc926ae90
Yoann <yoann.deblois@gmail.com>
2021-11-05 17:18:24 +0100

    style: added a blank line

--- LitReview/reviews/models.py
+++ LitReview/reviews/models.py
@@ -2,6 +2,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy, gettext as _
+
 from django_resized import ResizedImageField
 
 
