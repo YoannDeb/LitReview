71a62becdc1358542c2ca3b60c3ad15019a9a628
Yoann <yoann.deblois@gmail.com>
2021-11-06 15:35:19 +0100

    func: added all delete views

--- LitReview/reviews/urls.py
+++ LitReview/reviews/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('user_follows/', views.user_follows, name='user_follows'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', views.user_creation, name='signup'),
-    path('delete/<int:pk>', views.TicketDeleteView.as_view(), name='delete'),
-    path('success/', views.success, name='success')
+    path('delete_ticket/<int:pk>', views.TicketDeleteView.as_view(), name='delete_ticket'),
+    path('delete_review/<int:pk>', views.ReviewDeleteView.as_view(), name='delete_review'),
+    path('delete_follow/<int:pk>', views.UserFollowDeleteView.as_view(), name='delete_follow'),
 ]
