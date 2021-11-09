2daa09d628de70ab198fb44a39fae51d27d64fbc
Yoann <yoann.deblois@gmail.com>
2021-11-09 17:49:53 +0100

    func: secret key added

--- LitReview/LitReview/settings.py
+++ LitReview/LitReview/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-uvrcabhb+ww^1759@e*sx9!6m=2tcg1nw#3$h*3y98mlaz6tmv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.68']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
