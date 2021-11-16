864b17f89b7da4ad69e4354e0ec3066480f6d892
Yoann <yoann.deblois@gmail.com>
2021-11-16 17:52:02 +0100

    style: docstrings finalized.

--- LitReview/reviews/models.py
+++ LitReview/reviews/models.py
@@ -1,12 +1,16 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
-from django.utils.translation import gettext_lazy, gettext as _
+from django.utils.translation import gettext as _
 
 from django_resized import ResizedImageField
 
 
 class Ticket(models.Model):
+    """
+    Ticket model class, child of Model django's generic class.
+    Image is automatically resized using django_resized module.
+    """
     title = models.CharField(max_length=128)
     description = models.CharField(max_length=2048, blank=True)
     user = models.ForeignKey(
@@ -20,9 +24,12 @@ class Ticket(models.Model):
 
 
 class Review(models.Model):
+    """
+    Review model class, child of Model django's generic class.
+    Rating uses validators to ensure it is between 0 and 5.
+    """
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(
-        # validates that rating must be between 0 and 5
         validators=[MinValueValidator(0), MaxValueValidator(5)])
     headline = models.CharField(max_length=128)
     body = models.CharField(max_length=8192, blank=True)
@@ -36,6 +43,10 @@ class Review(models.Model):
 
 
 class UserFollow(models.Model):
+    """
+    User follow model class, child of Model django's generic class.
+    Subclass Meta ensures we don't get multiple UserFollows instances for unique user-user_followed pairs.
+    """
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following'
     )
@@ -47,8 +58,6 @@ class UserFollow(models.Model):
         return f"{self.user.username} abonné à {self.followed_user.username}"
 
     class Meta:
-        # ensures we don't get multiple UserFollows instances
-        # for unique user-user_followed pairs
         unique_together = ('user', 'followed_user', )
         verbose_name = _("Abonné à un utilisateur")
         verbose_name_plural = _("Abonnés aux utilisateurs")
